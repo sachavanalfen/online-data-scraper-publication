4de50e95acb3a488ec5b385cc1e4954d24f9e9c4
Sacha <sachavanalfen@gmail.com>
2019-12-08 19:34:04 +0100

    last changes

--- Twitch Dictionary/index.js
+++ Twitch Dictionary/index.js
@@ -1,3 +1,93 @@
+// TODO: add mouse over
+// TODO: check op safari
+// TODO: multiple
+var space = 300;
+var speed = 3;
+var direction = "left";
+
+// no need to change anything below this line
+/* --------------------------------- */
+
+//var modifier = (direction === "left") ? -1 : 1;
+
+
+// create a ticker div
+var tickerdiv = $("<div>").attr("id","ticker");
+tickerdiv.css({
+    "display":"inline-block",
+    "white-space": "nowrap",
+    "position":"relative"
+});
+
+// add it to the container
+$("#ticker-container").append(tickerdiv);
+$("#ticker-container").css("overflow","hidden");
+
+// move the ticker-text to the tickerdiv
+$("#ticker-text").css({
+    "display":"inline-block",
+    "position":"absolute"
+});
+tickerdiv.append(  $("#ticker-text") );
+
+
+// add ticker-texts untill the ticker-container is filled
+//var i = 1;
+var xpos    = $("#ticker-text").width() + space;
+var totalw  = $("#ticker-text").width() + $("#ticker-container").width();
+while (xpos < totalw ) {
+    var clone = $("#ticker-text").clone();
+    //clone.addClass("class-" + i);
+    clone.css({
+        "left":xpos,
+      //  "color": '#'+Math.floor(Math.random()*16777215).toString(16)
+    });
+    tickerdiv.append( clone );
+
+    xpos += clone.width() + space;
+   // i++;
+}
+//$("#ticker-text").addClass("class-0");
+
+animate();
+
+function animate() {
+
+    if (direction === "left") {
+        var farright = (tickerdiv.children().length-1) * ($("#ticker-text").width() + space) + space;
+
+        tickerdiv.children().each(function(){
+            var pos = $(this).position().left - speed;
+            $(this).css("left",pos);
+            if ($(this).position().left < -($("#ticker-text").width())) {
+                $(this).css("left",farright);
+            }
+        });
+    }
+    else {
+        //var farleft = tickerdiv.children().first().position().left;
+        var farleft = 0;
+
+        tickerdiv.children().each(function(){
+            var pos = $(this).position().left + speed;
+
+            //console.log(pos)
+            $(this).css("left",pos);
+            if ($(this).position().left > $("#ticker-container").width()) {
+                $(this).css("left", farleft - $("#ticker-text").width() - space );
+                farleft = $(this).position().left;
+            }
+        });
+    }
+
+
+    window.requestAnimationFrame(animate);
+}
+
+
+
+
+
 var spanClass = document.getElementsByClassName('class');
 var i = 0;
 while(i < spanClass.length){
@@ -27,7 +117,7 @@ while(i < spanCardType.length){
     spanCardType[i].onmouseover = function change(){
         var i = 0;
         while(i < spanCardType.length){
-            spanCardType[i].style.color = 'red';
+            spanCardType[i].style.color = 'rgb(234, 131, 255)';
             i++;
         }
     }
@@ -50,7 +140,7 @@ while(i < spanPlay.length){
     spanPlay[i].onmouseover = function change(){
         var i = 0;
         while(i < spanPlay.length){
-            spanPlay[i].style.color = 'green';
+            spanPlay[i].style.color = 'rgb(255, 0, 222)';
             i++;
         }
     }
@@ -73,7 +163,7 @@ while(i < spanHero.length){
     spanHero[i].onmouseover = function change(){
         var i = 0;
         while(i < spanHero.length){
-            spanHero[i].style.color = 'blue';
+            spanHero[i].style.color = 'rgb(22, 66, 255)';
             i++;
         }
     }
@@ -96,7 +186,7 @@ while(i < spanAHero.length){
     spanAHero[i].onmouseover = function change(){
         var i = 0;
         while(i < spanAHero.length){
-            spanAHero[i].style.color = 'orange';
+            spanAHero[i].style.color = 'rgb(205, 55, 181)';
             i++;
         }
     }
@@ -112,44 +202,156 @@ while(i < spanAHero.length){
     i++;
 }
 
-document.getElementById("tempDiv").onclick = function(e) {
-  e.target.style.visibility = 'hidden';
+
+var spanEmote = document.getElementsByClassName('Emote');
+var i = 0;
+while(i < spanEmote.length){
+    spanEmote[i].onmouseover = function change(){
+        var i = 0;
+        while(i < spanEmote.length){
+            spanEmote[i].style.color = 'rgb(62, 30, 153)';
+            i++;
+        }
+    }
+
+    spanEmote[i].onmouseout = function change(){
+        var i = 0;
+        while(i < spanEmote.length){
+            spanEmote[i].style.color = 'black';
+            i++;
+        }
+    }
+
+    i++;
 }
 
-document.getElementById("tempDiv1").onclick = function(e) {
-  e.target.style.visibility = 'hidden';
-}
 
-document.getElementById("tempDiv2").onclick = function(e) {
-  e.target.style.visibility = 'hidden';
-}
+// document.getElementById("tempDiv").onclick = function(e) {
+//   e.target.style.visibility = 'hidden';
+// }
+//
+// document.getElementById("tempDiv1").onclick = function(e) {
+//   e.target.style.visibility = 'hidden';
+// }
+//
+// document.getElementById("tempDiv2").onclick = function(e) {
+//   e.target.style.visibility = 'hidden';
+// }
+
+// get width & height of random pane
+var pane_width = $(".random-pane").width() - $(".grid-item").width();
+var pane_height = $(".random-pane").height() - $(".grid-item").height();
+
+// loop through all the items in the random-pane
+$(".random-pane").children().each( function(){
+
+  // get a random x position
+  var x = Math.round(Math.random() * pane_width);
+  // get a random y position
+  var y =  Math.round(Math.random() * pane_height);
+
+  // change the position of the item
+  $(this).css("top",y);
+  $(this).css("left",x);
+
+  // add hover functionality, to move item to the top on hover
+  $(this).on("mouseover",function(){
+    // this is a small hack, removing it and adding it again moves it to the top
+    $(this).parent().append(this);
+  });
+
+});
 
 
 
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-  // console.log(document.getElementById("myBtn").dataset.word);
-  modal.firstChild.append(btn.dataset.word);
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+//
+//
+//
+//
+// // TODO: add mouse over
+// // TODO: check op safari
+// // TODO: multiple
+// var space = 10;
+// var speed = 2;
+// var direction = "right";
+//
+// // no need to change anything below this line
+// /* --------------------------------- */
+//
+// //var modifier = (direction === "left") ? -1 : 1;
+//
+//
+// // create a ticker div
+// var tickerdiv = $("<div>").attr("id","ticker");
+// tickerdiv.css({
+//     "display":"inline-block",
+//     "white-space": "nowrap",
+//     "position":"relative"
+// });
+//
+// // add it to the container
+// $("#ticker-container2").append(tickerdiv);
+// $("#ticker-container2").css("overflow","hidden");
+//
+// // move the ticker-text to the tickerdiv
+// $("#ticker-text2").css({
+//     "display":"inline-block",
+//     "position":"absolute"
+// });
+// tickerdiv.append(  $("#ticker-text2") );
+//
+//
+// // add ticker-texts untill the ticker-container is filled
+// //var i = 1;
+// var xpos    = $("#ticker-text2").width() + space;
+// var totalw  = $("#ticker-text2").width() + $("#ticker-container2").width();
+// while (xpos < totalw ) {
+//     var clone = $("#ticker-text2").clone();
+//     //clone.addClass("class-" + i);
+//     clone.css({
+//         "left":xpos,
+//       //  "color": '#'+Math.floor(Math.random()*16777215).toString(16)
+//     });
+//     tickerdiv.append( clone );
+//
+//     xpos += clone.width() + space;
+//    // i++;
+// }
+// //$("#ticker-text").addClass("class-0");
+//
+// animate();
+//
+// function animate() {
+//
+//     if (direction === "left") {
+//         var farright = (tickerdiv.children().length-1) * ($("#ticker-text2").width() + space) + space;
+//
+//         tickerdiv.children().each(function(){
+//             var pos = $(this).position().left - speed;
+//             $(this).css("left",pos);
+//             if ($(this).position().left < -($("#ticker-text2").width())) {
+//                 $(this).css("left",farright);
+//             }
+//         });
+//     }
+//     else {
+//         //var farleft = tickerdiv.children().first().position().left;
+//         var farleft = 0;
+//
+//         tickerdiv.children().each(function(){
+//             var pos = $(this).position().left + speed;
+//
+//             //console.log(pos)
+//             $(this).css("left",pos);
+//             if ($(this).position().left > $("#ticker-container2").width()) {
+//                 $(this).css("left", farleft - $("#ticker-text2").width() - space );
+//                 farleft = $(this).position().left;
+//             }
+//         });
+//     }
+//
+//
+//     window.requestAnimationFrame(animate);
+// }
